3049af2a2ed3bba305433ba3cb5b06feda4cf8bc
hvd00dev <hvd00dev@gmail.com>
2025-03-21 19:23:48 -0300

    refactor: header when scroll
    - different style when scroll
    - added links on contact buttons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faInstagram,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const anchorClass =
-    "text-white text-sm sm:text-base font-semibold sm:font-bold hover:scale-[1.02] hover:transition-all hover:duration-200 cursor-pointer";
+    "text-white text-xs sm:text-sm font-semibold sm:font-bold hover:scale-105 transition-all duration-200 cursor-pointer";
 
   const contactClass =
-    "flex items-center justify-center w-8 h-8 max-lg:w-9 max-lg:h-9 text-white text-lg font-bold p-2 rounded-full border border-orange hover:cursor-pointer hover:scale-[1.03] hover:transition-transform hover:duration-100";
+    "bg-[#ff5e0081] backdrop-blur flex items-center justify-center w-8 h-8 max-lg:w-9 max-lg:h-9 text-white text-lg font-bold p-2 rounded-full border border-orange-500 hover:cursor-pointer hover:scale-[1.03] transition-transform duration-300";
 
   return (
-    <header className="w-full flex items-center justify-between">
+    <header
+      className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 transition-all duration-500 z-[5] ${
+        isScrolled
+          ? "py-3 opacity-100 translate-y-0"
+          : "bg-transparent py-6 opacity-90 translate-y-[-6px]"
+      }`}
+    >
       <Image src="/logo.png" alt="HVD logo" width={60} height={20} />
       <nav>
-        <ul className="flex items-center gap-12">
+        <ul
+          className={`flex items-center gap-12 transition-all duration-6 ${
+            isScrolled
+              ? "bg-black/30 backdrop-blur border border-[#36363650] px-4 py-3 rounded-full opacity-100 translate-y-0"
+              : "translate-y-[-5px]"
+          }`}
+        >
           <li>
             <a className={anchorClass} href="#">
               Home
@@ -37,17 +67,23 @@ export default function Header() {
         </ul>
       </nav>
       <div className="flex items-center gap-2">
-        <a className={contactClass}>
-          {" "}
+        <a className={contactClass}
+        href="https://github.com/hvd00dev"
+        target="_blank"
+        >
           <FontAwesomeIcon icon={faGithub} />
         </a>
-        <a className={contactClass}>
-          {" "}
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a className={contactClass}>
-          {" "}
+        <a
+          className={contactClass}
+          href="https://www.linkedin.com/in/hudson-ferreira-483936186/"
+          target="_blank"
+        >
           <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a className={contactClass}
+         href="https://www.instagram.com/hudfrr/"
+         target="_blank">
+          <FontAwesomeIcon icon={faInstagram} />
         </a>
       </div>
     </header>
