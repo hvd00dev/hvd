0c73078265aa481e72abe27294f381bd9a38e089
hvd00dev <hvd00dev@gmail.com>
2025-03-21 17:06:37 -0300

    refactor: header component
    -styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-8 font-[family-name:var(--font-geist-sans)]">
       <main className="w-full flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Header/>
-        <h1>hvd</h1>
+        <Header />
       </main>
       <footer></footer>
     </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,46 @@
 import Image from "next/image";
 import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Header() {
+  const anchorClass =
+    "text-white text-sm sm:text-base font-semibold sm:font-bold hover:scale-[1.02] hover:transition-all hover:duration-200 cursor-pointer";
+
+  const contactClass =
+    "flex items-center justify-center w-8 h-8 max-lg:w-9 max-lg:h-9 text-white text-lg font-bold p-2 rounded-full border border-orange hover:cursor-pointer hover:scale-[1.03] hover:transition-transform hover:duration-100";
+
   return (
     <header className={styles.header}>
-       <Image
-        src="/logo.png"
-        alt="HVD logo"
-        width={100}
-        height={20}
-        />
+      <Image src="/logo.png" alt="HVD logo" width={60} height={20} />
       <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
+        <ul className="flex items-center gap-2">
+          <li>
+            <a className={anchorClass} href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className={anchorClass} href="#">
+              About
+            </a>
+          </li>
         </ul>
       </nav>
+      <div className="flex items-center gap-2">
+        <a className={contactClass}>
+          {" "}
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a className={contactClass}>
+          {" "}
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a className={contactClass}>
+          {" "}
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
     </header>
   );
 }
