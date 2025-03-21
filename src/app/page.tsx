1c95d79591a8da6dfd80aeda88ebef6d31b8e88d
hvd00dev <hvd00dev@gmail.com>
2025-03-21 20:50:40 -0300

    feat: hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[0px_1fr_20px] min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="w-full flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Header />
+        <Hero />
       </main>
       <footer></footer>
     </div>
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,5 @@
+export default function Hero() {
+  return <div className="w-full bg-white h-[1000px]">
+
+  </div>;
+}
