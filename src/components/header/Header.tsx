08cfe13e70470f21e0ab6a1e28212147f6232022
hvd00dev <hvd00dev@gmail.com>
2025-03-21 17:17:56 -0300

    refactor: header fixes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "./Header.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
@@ -11,10 +10,10 @@ export default function Header() {
     "flex items-center justify-center w-8 h-8 max-lg:w-9 max-lg:h-9 text-white text-lg font-bold p-2 rounded-full border border-orange hover:cursor-pointer hover:scale-[1.03] hover:transition-transform hover:duration-100";
 
   return (
-    <header className={styles.header}>
+    <header className="w-full flex items-center justify-between">
       <Image src="/logo.png" alt="HVD logo" width={60} height={20} />
       <nav>
-        <ul className="flex items-center gap-2">
+        <ul className="flex items-center gap-12">
           <li>
             <a className={anchorClass} href="#">
               Home
@@ -23,6 +22,16 @@ export default function Header() {
           <li>
             <a className={anchorClass} href="#">
               About
+            </a>
+          </li>
+          <li>
+            <a className={anchorClass} href="#">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a className={anchorClass} href="#">
+              Contact
             </a>
           </li>
         </ul>
