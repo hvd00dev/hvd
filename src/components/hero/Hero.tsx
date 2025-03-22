186e8640f779e7cf6db86e02282cd205ea5f9c81
hvd00dev <hvd00dev@gmail.com>
2025-03-22 02:16:25 -0300

    refactor: hero apresentation

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,28 @@
-export default function Hero() {
-  return <div className="w-full bg-white h-[1000px]">
+import Image from "next/image";
+import MaxWidth from "../maxwidth/maxwidth";
 
-  </div>;
+export default function Hero() {
+  return (
+    <MaxWidth>
+      <div className="text-[#ededed] w-full pt-[26px]">
+        <div className="flex items-center justify-center gap-[20px] flex-col px-4 py-6 w-full bg-[#121212] rounded-[10px]">
+          <div className="flex flex-col items-center justify-center">
+          <h1 className="text-[40px] font-bold">Hi, I'm Hudson! 👋</h1>
+          <span className="text-[26px] font-semibold">
+            A Front-end Developer 👨‍💻
+          </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="whitespace-nowrap">⚡ +2 years of experience</span>
+            <span className="whitespace-nowrap">
+              ・ 🏡 Rio de Janeiro, Brazil
+            </span>
+            <span className="whitespace-nowrap">
+              ・ 🌎 Portuguese and English
+            </span>
+          </div>
+        </div>
+      </div>
+    </MaxWidth>
+  );
 }
