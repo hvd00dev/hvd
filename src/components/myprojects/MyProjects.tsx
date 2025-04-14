0923ca8599419a71c785db9ba2100544717033b7
hvd00dev <hvd00dev@gmail.com>
2025-04-13 21:14:58 -0300

    refactor: section tag on projects

--- src/components/myprojects/MyProjects.tsx
+++ src/components/myprojects/MyProjects.tsx
@@ -5,17 +5,18 @@ import { faCode } from "@fortawesome/free-solid-svg-icons";
 export default function MyProjects() {
   return (
     <MaxWidth>
-      <div className="flex flex-col gap-4">
-        <div className="flex items-center gap-2 text-[22px]">
-          <span className="text-orange-500">
-            <FontAwesomeIcon icon={faCode} />
-          </span>
-          <h3 className="font-bold">Latest projects</h3>
-        </div>
-        <div className="flex items-center justify-center gap-2 h-[600px] bg-[#000] rounded-xl">
+      <section>
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-2 text-[22px]">
+            <span className="text-orange-500">
+              <FontAwesomeIcon icon={faCode} />
+            </span>
+            <h3 className="font-bold">Latest projects</h3>
+          </div>
+
           <span className="text-[#474747]">under development</span>
         </div>
-      </div>
+      </section>
     </MaxWidth>
   );
 }
