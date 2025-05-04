e7f4ba621e23fdaae3f543667b0f34ce71be05af
hvd00dev <hvd00dev@gmail.com>
2025-05-04 16:41:06 -0300

    refactor: starting projects component

--- src/components/sections/home/myprojects/MyProjects.tsx
+++ src/components/sections/home/myprojects/MyProjects.tsx
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MaxWidth from "../../../layout/maxwidth/maxwidth";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
+import Button from "@/components/ui/button/Button";
 
 export default function MyProjects() {
   return (
     <MaxWidth>
       <section id="projects">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 mb-10">
           <div className="flex items-center gap-2 text-[22px]">
             <span className="text-orange-500">
               <FontAwesomeIcon icon={faCode} />
@@ -15,6 +16,10 @@ export default function MyProjects() {
           </div>
 
           <span className="text-[#474747]">under development</span>
+          <div className="w-full flex justify-center items-center">
+
+          <Button text="See all projects" href="projects"></Button>
+          </div>
         </div>
       </section>
     </MaxWidth>
