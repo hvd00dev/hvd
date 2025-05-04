f1228e87f10980cfbf9a16a51ebc846a04aa19b5
hvd00dev <hvd00dev@gmail.com>
2025-05-04 16:53:03 -0300

    fix: build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,20 @@
 "use client";
 
 import "./globals.css";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import About from "@/components/sections/home/about/About";
 import ContactForm from "@/components/sections/home/contact/ContactForm";
 import Hero from "@/components/sections/home/hero/Hero";
 import MyProjects from "@/components/sections/home/myprojects/MyProjects";
+import ScrollHandler from "@/components/ui/scrollhandler/ScrollHandler";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const scrollTo = searchParams.get("scrollTo");
-    if (scrollTo) {
-      const element = document.getElementById(scrollTo);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, [searchParams]);
-
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="w-full flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Suspense fallback={null}>
+          <ScrollHandler />
+        </Suspense>
         <Hero />
         <About />
         <MyProjects />
